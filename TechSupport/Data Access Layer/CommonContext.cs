45430ee35aea35bf6077848f4c0233671dcb39e9
aliardan <aliardanchik@gmail.com>
2021-04-30 19:28:24 +0300

    feat/adding Data Access Layer

---
+++ TechSupport/Data Access Layer/CommonContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace TechSupport.Data_Access_Layer
+{
+    public class CommonContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public CommonContext(IOptions<CommonContextOptions> options)
+        {
+            _connectionString = options.Value.ConnectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(_connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Models.Task>();
+        }
+
+        public DbSet<Models.Task> Tasks { get; set; }
+    }
+}
