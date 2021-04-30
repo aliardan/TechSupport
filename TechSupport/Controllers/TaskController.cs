88609b1fb4db670753f95d43b5c1f15d2f969967
aliardan <aliardanchik@gmail.com>
2021-04-30 20:21:30 +0300

    feat/adding Base Implementation

---
+++ TechSupport/Controllers/TaskController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TechSupport.Data_Access_Layer;
+using TechSupport.Models;
+
+namespace TechSupport.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class TaskController : Controller
+    {
+        private readonly CommonContext _commonContext;
+
+        public TaskController(CommonContext commonContext)
+        {
+            _commonContext = commonContext;
+        }
+
+        public object Task()
+        {
+            var items = _commonContext.Tasks.ToList();
+            return items;
+        }
+    }
+}
