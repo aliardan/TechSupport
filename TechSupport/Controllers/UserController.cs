c7e87dc1f9026f8e3d8d4bbaed8a2f1c663be58d
aliardan <aliardanchik@gmail.com>
2021-05-08 22:37:44 +0300

    feat/adding an additional logic for controllers

---
+++ TechSupport/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechSupport.Data_Access_Layer;
+
+namespace TechSupport.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly CommonContext _commonContext;
+
+        public UserController(CommonContext commonContext)
+        {
+            _commonContext = commonContext;
+        }
+
+        [Authorize(Roles = "User, Admin")]
+        public IActionResult UserPrivatePage()
+        {
+            var user = HttpContext.User;
+            var tasks = _commonContext.Tasks.ToList();
+            return View("Pages/UserPrivatePage.cshtml", tasks);
+        }
+
+        public IActionResult CreateTask()
+        {
+            return View("Pages/CreateTask.cshtml");
+        }
+    }
+}
