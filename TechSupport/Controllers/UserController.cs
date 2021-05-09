5c52c8a699704c084468c06e64e502d2ea70e8d6
aliardan <aliardanchik@gmail.com>
2021-05-10 00:33:11 +0300

    feat/additional logic for UserController

--- TechSupport/Controllers/UserController.cs
+++ TechSupport/Controllers/UserController.cs
@@ -1,7 +1,13 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TechSupport.Data_Access_Layer;
+using TechSupport.Models;
 
 namespace TechSupport.Controllers
 {
@@ -22,9 +28,41 @@ namespace TechSupport.Controllers
             return View("Pages/UserPrivatePage.cshtml", tasks);
         }
 
+        [Authorize(Roles = "Customer, Admin")]
+        public IActionResult UserPrivatePageForm()
+        {
+            var user = HttpContext.User;
+            var tasks = _commonContext.Tasks.ToList();
+            return View("Pages/UserPrivatePageForm.cshtml", tasks);
+        }
+
         public IActionResult CreateTask()
         {
             return View("Pages/CreateTask.cshtml");
+        }
+
+        [Authorize(Roles = "Customer, Admin")]
+        [HttpPost]
+        public IActionResult CreateTask(CreateTaskData createTaskData)
+        {
+            var existingTask = _commonContext
+                .Tasks
+                .FirstOrDefault(x => x.Name == createTaskData.Name);
+
+            if (existingTask != null)
+            {
+                return View("Pages/CreateTask.cshtml");
+            }
+
+            var newTask = new Task
+            {
+                Name = createTaskData.Name,
+                Description = createTaskData.Description
+            };
+            _commonContext.Tasks.Add(newTask);
+            _commonContext.SaveChanges();
+            
+            return View("Pages/Index.cshtml");
         }
     }
 }
